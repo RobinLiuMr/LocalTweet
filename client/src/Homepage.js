b4a3efefa9dcd7cb499c34aae663f1de7ac9b8d3
Robin Liu <robin.liu.mr@gmail.com>
2022-09-10 19:59:35 +0200

    style tag and more button

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -48,12 +48,12 @@ export default function Homepage() {
 
     return (
         <div className="bg-gray-900 pb-32">
-            <div className="px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
-                <div className="flex flex-row justify-center bg-gray-700 rounded-full">
+            <div className="px-4 pt-8 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
+                <div className="flex flex-row justify-start items-center bg-gray-700 rounded-full pl-4">
                     <img className="w-8 h-8" src="/twitter-48.png"></img>
                     {/* modified on https://www.hyperui.dev/components/application-ui/inputs */}
 
-                    <form onSubmit={onSearch} className="relative">
+                    <form onSubmit={onSearch} className="relative w-full mr-6">
                         <input
                             className="w-full py-3 pl-3 pr-12 text-sm rounded bg-gray-700"
                             id="city"
@@ -68,13 +68,15 @@ export default function Homepage() {
 
             <main className="text-white bg-gray-900">
                 {/* modified on https://www.hyperui.dev/components/application-ui/button-groups */}
-                <div className="flex justify-around items-center text-xs -space-x-px rounded-md lg:hidden">
+                <div className="px-4 py-4 max-w-screen-xl flex justify-between items-center text-xs -space-x-px rounded-md lg:hidden">
                     {categories.map((category, index) => (
                         <button
                             key={category}
                             onClick={() => setCategoryIndex(index)}
-                            className={`px-5 py-3 font-medium border rounded-md hover:z-10 focus:outline-none focus:border-indigo-600 focus:z-10 hover:bg-[#55acee] active:opacity-75 ${
+                            className={`px-4 py-2 font-medium text-gray-400 bg-gray-700 rounded-full focus:outline-none focus:text-white-600 focus:z-10 active:opacity-75 ${
                                 index == categoryIndex ? "bg-[#55acee]" : ""
+                            } active:opacity-75 ${
+                                index == categoryIndex ? "text-gray-900" : ""
                             }`}
                             type="button"
                         >
@@ -86,9 +88,9 @@ export default function Homepage() {
                 {/* modified on https://www.hyperui.dev/components/marketing/sections */}
 
                 <section>
-                    <div className="px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
+                    <div className="px-4 py-4 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
                         <div className="max-w-lg mx-auto text-center">
-                            <h2 className="text-3xl font-bold sm:text-4xl">
+                            <h2 className="text-2xl font-bold lg:text-4xl">
                                 Wichtigste lokale Informationen auf Twitter
                             </h2>
                         </div>
@@ -117,10 +119,10 @@ export default function Homepage() {
                             categoryIndex={2}
                         />
 
-                        <div className="mt-12 text-center">
+                        <div className="text-center">
                             <a
                                 onClick={onMore}
-                                className="inline-flex items-center px-8 py-3 mt-8 text-white bg-pink-600 border border-pink-600 rounded hover:bg-transparent active:text-pink-500 focus:outline-none focus:ring"
+                                className="inline-flex items-center px-8 py-3 mt-8 text-gray-400 bg-gray-700 rounded-full hover:bg-transparent active:bg-[#55acee] active:text-gray-900 focus:outline-none focus:ring"
                                 href="#"
                             >
                                 <span className="text-sm font-medium">
