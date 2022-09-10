988a0e003045f8eb7c05ae177b9b55c3e96f10b6
Robin Liu <robin.liu.mr@gmail.com>
2022-09-10 18:25:51 +0200

    style search area

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -47,48 +47,24 @@ export default function Homepage() {
     }
 
     return (
-        <div>
-            <header className="flex flex-row justify-center">
-                {/* modified on https://www.hyperui.dev/components/application-ui/inputs */}
-                <label
-                    className="relative block p-3 rounded-lg lg:w-1/5"
-                    htmlFor="city"
-                >
-                    <span
-                        className="text-md font-medium text-gray-500"
-                        htmlFor="name"
-                    >
-                        Stadt
-                    </span>
-                    <input
-                        className="w-full p-0 text-md border-none focus:ring-0"
-                        id="city"
-                        value={searchTerm}
-                        onChange={handelChange}
-                        type="text"
-                        placeholder="Frankfurt"
-                    />
-                </label>
+        <div className="bg-gray-900 pb-32">
+            <div className="px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
+                <div className="flex flex-row justify-center bg-gray-700 rounded-full">
+                    <img className="w-8 h-8" src="/twitter-48.png"></img>
+                    {/* modified on https://www.hyperui.dev/components/application-ui/inputs */}
 
-                {/* modified on https://www.hyperui.dev/components/marketing/buttons */}
-                <a
-                    onClick={onSearch}
-                    className="inline-flex items-center px-2 py-2 text-md font-medium text-white transition-colors bg-[#55acee]  rounded hover:bg-transparent hover:text-[#55acee] focus:outline-none focus:ring active:opacity-75"
-                    href="#"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Jetzt sehen
-                    <svg
-                        className="w-5 h-5 ml-2"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                        aria-hidden="true"
-                    >
-                        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                    </svg>
-                </a>
-            </header>
+                    <form onSubmit={onSearch} className="relative">
+                        <input
+                            className="w-full py-3 pl-3 pr-12 text-sm rounded bg-gray-700"
+                            id="city"
+                            value={searchTerm}
+                            onChange={handelChange}
+                            type="search"
+                            placeholder="Stadt suchen"
+                        />
+                    </form>
+                </div>
+            </div>
 
             <main className="text-white bg-gray-900">
                 {/* modified on https://www.hyperui.dev/components/application-ui/button-groups */}
@@ -113,7 +89,7 @@ export default function Homepage() {
                     <div className="px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
                         <div className="max-w-lg mx-auto text-center">
                             <h2 className="text-3xl font-bold sm:text-4xl">
-                                Local most important information on Twitter
+                                Wichtigste lokale Informationen auf Twitter
                             </h2>
                         </div>
 
